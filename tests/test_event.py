b1511707b2f26113914d3f2a2670daa89f4f18ff
rwhitt2049 <rwhitt2049@gmail.com>
2016-01-04 21:05:34 -0600

    Complete initial unit test cases

--- tests/test_event.py
+++ tests/test_event.py
@@ -7,32 +7,56 @@ from unittest import TestCase
 class TestEventDetection(TestCase):
     def test_default_parameters(self):
         """
-        Test event detection with only a supplied condtion
+        Test event detection with only a supplied condition
         """
         np.random.seed(10)
         validation_array = np.random.random_integers(0, 1, 100)
         condition = (validation_array > 0)
         events = Event(condition)
+        
         npt.assert_aray_equal(validation_array, events.array)
-    #def test_no_events_found(self):
+        
+    def test_no_events_found(self):
         """
         Test arrays that have no active events
         """
+        validation_array = np.zeros(10, dtype='i1')
+        condition = (validation_array > 0)
+        events = Event(condition)
+        
+        npt.assert_aray_equal(validation_array, events.array)
 
-    #def test_event_always_active(self):
+    def test_event_always_active(self):
         """
         Test arrays that has an event active throughout entire array
         """
+        validation_array = np.ones(10, dtype='i1')
+        condition = (validation_array > 0)
+        events = Event(condition)
+        
+        npt.assert_aray_equal(validation_array, events.array)
 
-    #def test_array_with_event_active_at_start(self):
+    def test_array_with_event_active_at_start(self):
         """
         Test arrays that have events active at the start
         """
+        validation_array = np.zeros(10, dtype='i1')
+        validation_array[:1] = 1
+        condition = (validation_array > 0)
+        
+        events = Event(condition)
+        npt.assert_aray_equal(validation_array, events.array)
 
-    #def test_array_with_event_active_at_end(self):
+    def test_array_with_event_active_at_end(self):
         """
         Test arrays that have events active at the end
         """
+        validation_array = np.zeros(10, dtype='i1')
+        validation_array[-1:] = 1
+        condition = (validation_array > 0)
+        
+        events = Event(condition)
+        npt.assert_aray_equal(validation_array, events.array)
 
     #def test_event_entry_debounce(self):
     #def test_event_exit_debounce(self):
