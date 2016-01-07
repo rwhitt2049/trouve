4a09b21a00be89b37603bdf29a65e7b02318cf77
rwhitt2049 <rwhitt2049@gmail.com>
2016-01-06 21:06:36 -0600

    Remove set_trace() statements

--- nimble/core/event_detection.py
+++ nimble/core/event_detection.py
@@ -24,7 +24,7 @@ class Event(object):
         Return the found events as a numpy array of 0's and 1'sample_rate
         """
         #TODO - Cache this value?
-        #import pdb; pdb.set_trace()
+
         output = np.zeros(self.condition.size, dtype='i1')
         for start, stop in zip(self.starts, self.stops):
             output[start:stop] = 1
@@ -53,7 +53,7 @@ class Event(object):
             to_end = np.array([-1])
 
         deltas = np.ediff1d(mask, to_begin=to_begin, to_end=to_end)
-        #import pdb; pdb.set_trace()
+
         starts = np.ma.masked_where(deltas < 1, slice_index).compressed()
         stops = np.ma.masked_where(deltas > -1, slice_index).compressed()
 
