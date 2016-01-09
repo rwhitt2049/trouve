78b03efe7c2595794a32f28b6e6177132817d5a9
rwhitt2049 <rwhitt2049@gmail.com>
2016-01-08 22:51:56 -0600

    Add debounce code
    Passing unittests

--- nimble/core/event_detection.py
+++ nimble/core/event_detection.py
@@ -2,8 +2,9 @@ import numpy as np
 
 
 class Event(object):
-    def __init__(self, condition, entry_debounce=0, exit_debounce=0,
-                 sample_rate=1):
+    def __init__(self, condition, sample_rate=1,
+                 entry_debounce=0, exit_debounce=0):
+
         self.condition = condition
         self.entry_debounce = entry_debounce
         self.exit_debounce = exit_debounce
@@ -33,8 +34,8 @@ class Event(object):
     def _apply_parameters(self):
         starts, stops = self._apply_condition()
         
-        if self.entry_debounce or self.exit_debounce:
-            starts, stops = self._apply_debounce()
+        if len(starts) and (self.entry_debounce or self.exit_debounce):
+            starts, stops = self._apply_debounce(starts, stops)
 
         return starts, stops
 
@@ -62,5 +63,41 @@ class Event(object):
 
         return starts, stops
         
-    def _apply_debounce(self):
-        pass
+    def _apply_debounce(self, starts, stops):
+        #n_events = len(starts)
+        start_mask = np.zeros(starts.size)
+        stop_mask = np.zeros(stops.size)
+        event_started = False
+
+        for id in np.arange(starts.size):
+            event_length = stops[id] - starts[id]
+
+            try:
+                reset_length = starts[id+1] - stops[id]
+            except IndexError:
+                reset_length = None
+
+            if event_length >= self.entry_debounce and not event_started:
+                event_started = True
+            elif not event_started and event_length < self.entry_debounce:
+                start_mask[id] = 1
+                stop_mask[id] = 1
+            elif event_started:
+                pass
+            else:
+                raise ValueError
+
+            if not event_started or reset_length is None:
+                pass
+            elif event_started and reset_length >= self.exit_debounce:
+                event_started = False
+            elif event_started and reset_length < self.exit_debounce:
+                start_mask[id + 1] = 1
+                stop_mask[id] = 1
+            else:
+                raise ValueError
+
+        starts = np.ma.masked_where(start_mask > 0, starts).compressed()
+        stops = np.ma.masked_where(stop_mask > 0, stops).compressed()
+
+        return starts, stops
