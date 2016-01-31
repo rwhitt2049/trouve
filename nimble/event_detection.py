9d5466bad05a49042f8bd65e5fe51f25c8b2e31c
rwhitt2049 <rwhitt2049@gmail.com>
2016-01-30 21:56:44 -0600

    Remove trouble shooting print statements

--- nimble/event_detection.py
+++ nimble/event_detection.py
@@ -144,10 +144,8 @@ class Events(object):
         # TODO - Replace with function in Cython for significant speed boost
         try:
             import nimble.cyfunc.debounce as cy
-            print('using cython')
             starts, stops = cy.debounce(starts, stops, self.entry_debounce, self.exit_debounce)
         except ImportError:
-            print('not using cython')
             start_mask = np.zeros(starts.size)
             stop_mask = np.zeros(stops.size)
             event_active = False
