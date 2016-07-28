723ba8b891892ebf691cfca045aa74a012387a54
rwhitt2049 <rwhitt2049@gmail.com>
2016-07-27 23:48:55 -0500

    Add test for small increments in debounces

--- tests/test_debounce.py
+++ tests/test_debounce.py
@@ -35,6 +35,15 @@ class TestPyDebounceFunction(TestCase):
         npt.assert_array_equal(starts_validation, starts)
         npt.assert_array_equal(stops_validation, stops)
 
+    def test_small_deb(self):
+        starts_validation = np.array([6])
+        stops_validation = np.array([9])
+        starts, stops = py.debounce(np.array([2, 6, 10]), np.array([4, 9, 11]),
+                                    2.000001, 0.999999)
+
+        npt.assert_array_equal(starts_validation, starts)
+        npt.assert_array_equal(stops_validation, stops)
+
 
 class TestCyDebounceFunction(TestCase):
     # condition = [0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1]
@@ -61,6 +70,15 @@ class TestCyDebounceFunction(TestCase):
         stops_validation = np.array([4, 11])
         starts, stops = cy.debounce(np.array([2, 6, 10]), np.array([4, 9, 11]),
                                     2, 2)
+
+        npt.assert_array_equal(starts_validation, starts)
+        npt.assert_array_equal(stops_validation, stops)
+
+    def test_small_deb(self):
+        starts_validation = np.array([6])
+        stops_validation = np.array([9])
+        starts, stops = py.debounce(np.array([2, 6, 10]), np.array([4, 9, 11]),
+                                    2.000001, 0.999999)
 
         npt.assert_array_equal(starts_validation, starts)
         npt.assert_array_equal(stops_validation, stops)
