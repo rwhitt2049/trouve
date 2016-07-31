331a75ca6bf947d17dd3ff3bf7d8b4f6be7a9f9d
rwhitt2049 <rwhitt2049@gmail.com>
2016-07-31 11:22:21 -0500

    Update unit test_iterable_event.py to handle new logic

--- tests/test_iterable_event.py
+++ tests/test_iterable_event.py
@@ -17,17 +17,17 @@ class TestClassIterable(TestCase):
         test_stops = []
 
         for event in self.events:
-            test_starts.append(event.start)
-            test_stops.append(event.stop)
+            test_starts.append(event.istart)
+            test_stops.append(event.istop)
 
-        npt.assert_array_equal(self.events.starts, test_starts)
-        npt.assert_array_equal(self.events.stops, test_stops)
+        npt.assert_array_equal([1, 7, 10], test_starts)
+        npt.assert_array_equal([3, 8, 11], test_stops)
 
     def test_durations(self):
         validation_durations = [1.5, 1, 1]
         test_durations = []
         for event in self.events:
-            test_durations.append(event.duration)
+            test_durations.append(event.iduration)
 
         npt.assert_array_equal(validation_durations, test_durations)
 
