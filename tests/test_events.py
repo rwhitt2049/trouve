81802afcb20e2ee24867ed5107012f159080d56b
rwhitt2049 <rwhitt2049@gmail.com>
2016-09-12 22:42:15 -0500

    Add unit tests for as_array(), as_series(), properties and attributes.

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,11 +1,13 @@
 import numpy as np
 import numpy.testing as npt
+import pandas as pd
 from unittest import TestCase
 from nimble import Events
 
 
 class EvTestCase(TestCase):
-    def assertStartStops(self, events, vstarts, vstops):
+    @staticmethod
+    def assertStartStops(events, vstarts, vstops):
         npt.assert_array_equal(events._starts, vstarts)
         npt.assert_array_equal(events._stops, vstops)
 
@@ -195,5 +197,165 @@ class TestEventOffset(EvTestCase):
         self.assertStartStops(events, vstarts, vstops)
 
 
-# Test constructor
-    #property tests
+class TestAsArrayMethod(TestCase):
+    def setUp(self):
+        conditional_array = np.array([0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1])
+        condition = (conditional_array > 0)
+        self.events = Events(condition, period=1).find()
+
+    def test_default_parameters(self):
+        """Test as_array() with default settings"""
+        validation_array = np.array([0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1])
+        npt.assert_array_equal(validation_array, self.events.as_array())
+
+    def test_as_array_false_value(self):
+        """Test as_array() with low value"""
+        validation_array = np.array([-1, 1, 1, 1, -1, -1, -1, 1, 1, -1, 1, 1])
+        npt.assert_array_equal(validation_array, self.events.as_array(
+            false_values=-1))
+
+    def test_as_array_true_value(self):
+        """Test as_array() with high value"""
+        validation_array = np.array([0, 5, 5, 5, 0, 0, 0, 5, 5, 0, 5, 5])
+        npt.assert_array_equal(validation_array, self.events.as_array(
+            true_values=5))
+
+    def test_as_array_false_and_true_value(self):
+        """Test as_array() with low and high values"""
+        validation_array = np.array([-1, 5, 5, 5, -1, -1, -1, 5, 5, -1, 5, 5])
+        npt.assert_array_equal(validation_array, self.events.as_array(
+            false_values=-1,
+            true_values=5))
+
+    def test_type(self):
+        typ = type(self.events.as_array(false_values=-1, true_values=5))
+        self.assertEqual(typ, np.ndarray)
+
+
+class TestAsSeries(TestCase):
+    def setUp(self):
+        conditional_array = np.array([0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1])
+        condition = (conditional_array > 0)
+        self.events = Events(condition, period=1).find()
+
+    def test_default_parameters(self):
+        """Test as_array() with default settings"""
+        validation_series = pd.Series([0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1])
+        npt.assert_array_equal(validation_series, self.events.as_series())
+
+    def test_as_array_false_value(self):
+        """Test as_array() with low value"""
+        validation_series = np.array([-1, 1, 1, 1, -1, -1, -1, 1, 1, -1, 1, 1])
+        npt.assert_array_equal(validation_series, self.events.as_series(
+            false_values=-1))
+
+    def test_as_array_true_value(self):
+        """Test as_array() with high value"""
+        validation_series = np.array([0, 5, 5, 5, 0, 0, 0, 5, 5, 0, 5, 5])
+        npt.assert_array_equal(validation_series, self.events.as_series(
+            true_values=5))
+
+    def test_as_array_false_and_true_value(self):
+        """Test as_array() with low and high values"""
+        validation_series = np.array([-1, 5, 5, 5, -1, -1, -1, 5, 5, -1, 5, 5])
+        npt.assert_array_equal(validation_series, self.events.as_series(
+            false_values=-1,
+            true_values=5))
+
+    def test_type(self):
+        typ = type(self.events.as_series(false_values=-1, true_values=5))
+        self.assertEqual(typ,  pd.core.series.Series)
+
+
+class TestDurations(TestCase):
+    def setUp(self):
+        condition_array = np.array([1, 0, 1, 1, 1, 1, 0, 0, 1, 1,
+                                    0, 0, 0, 1, 0, 0, 0, 1, 0, 0])
+
+        condition = (condition_array > 0)
+        self.events = Events(condition, period=1/3,
+                             adeb=0.5, ddeb=1).find()
+
+    def test_durations(self):
+        # validation_array = np.array([0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+        #                              0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        validation_durations = [(8 / 3)]
+        npt.assert_array_equal(validation_durations, self.events.durations)
+
+
+class TestEventDetection(TestCase):
+    def test_default_parameters(self):
+        """Test event detection with only a supplied condition"""
+        np.random.seed(10)
+        validation_array = np.random.random_integers(0, 1, 100)
+        condition = (validation_array > 0)
+        events = Events(condition, period=1).find()
+
+        npt.assert_array_equal(validation_array, events.as_array())
+
+    def test_multi_input_condition_event(self):
+        """Test arrays that have multi-input conditions"""
+        x = np.array([0, 1, 1, 1, 0, 0, 0, 1, 1, 0])
+        y = np.array([0, 0, 1, 1, 1, 0, 0, 1, 0, 1])
+
+        validation_array = np.array([0, 0, 1, 1, 0, 0, 0, 1, 0, 0])
+        condition = ((x > 0) & (y > 0))
+        events = Events(condition, period=1).find()
+
+        npt.assert_array_equal(validation_array, events.as_array())
+
+
+class TestSpecialMethods(TestCase):
+    def setUp(self):
+        condition_array = np.array([1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1])
+        self.condition = (condition_array > 0)
+        self.events = Events(self.condition, period=1).find()
+
+    def test__len__(self):
+        self.assertEquals(4, len(self.events))
+
+    def test__eq__(self):
+        other = Events(self.condition, period=1).find()
+        self.assertEqual(self.events, other)
+
+
+class TestAttributes(TestCase):
+    def setUp(self):
+        condition_array = np.array([1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1])
+        self.condition = (condition_array > 0)
+
+    def test_period(self):
+        self.assertRaises(ValueError, Events, self.condition, period=0)
+
+    def test_startoffset(self):
+        self.assertRaises(ValueError, Events, self.condition,
+                          period=1, startoffset=1)
+
+    def test_stopoffset(self):
+        self.assertRaises(ValueError, Events, self.condition, period=0, stopoffset=-1)
+
+
+class TestProperties(TestCase):
+    def setUp(self):
+        self.events = Events(np.array([False, False]), period=0.12,
+                             adeb=1, ddeb=1,
+                             mindur=1, maxdur=1,
+                             startoffset=-1, stopoffset=1)
+
+    def test_adeb(self):
+        self.assertEqual(self.events._adeb, 9)
+
+    def test_ddeb(self):
+        self.assertEqual(self.events._adeb, 9)
+
+    def test_mindur(self):
+        self.assertEqual(self.events._mindur, 9)
+
+    def test_maxdur(self):
+        self.assertEqual(self.events._maxdur, 8)
+
+    def test_startoffset(self):
+        self.assertEqual(self.events._startoffset, -9)
+
+    def test_stopoffset(self):
+        self.assertEqual(self.events._stopoffset, 9)
