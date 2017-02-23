df438fdbd5205420efcbcd284cc93e7e4e214229
rwhitt2049 <rwhitt2049@gmail.com>
2017-02-23 01:22:19 -0600

    Update gitignore

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ kwargs = dict(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering'
     ],
     keywords=['time_series', 'timeseries', 'iot', 'sensor'],
