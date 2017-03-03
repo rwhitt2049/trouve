e458459a8b06a8db7aa715ac5cf45752b4adbb6b
rwhitt2049 <rwhitt2049@gmail.com>
2017-03-02 20:50:05 -0600

    Remove dead code, update TODO's

--- trouver/__init__.py
+++ trouver/__init__.py
@@ -2,3 +2,10 @@ from trouver.find_events import find_events
 from trouver.transformations import *
 
 __version__ = '0.4.dev7'
+
+# TODO - Finish documentation
+# TODO - move input checking from subfunctions to wrapper functions, _debounce to debounce
+# TODO - standardize argument names: ie mindur and maxdur should be min_duration, max_duration
+# TODO - remove all main functions
+# TODO - Update readme
+# TODO - Add logging
