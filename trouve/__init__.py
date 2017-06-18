76ea5d439c940d955d26ca2c118292644d1fde25
rwhitt2049 <rwhitt2049@gmail.com>
2017-06-18 00:01:42 -0500

    Rev to 0.5.0-alpha.1

--- trouve/__init__.py
+++ trouve/__init__.py
@@ -1,6 +1,6 @@
 from trouve.find_events import find_events
 
-__version__ = '0.4.4'
+__version__ = '0.5.0-alpha.1'
 
 # TODO - move input checking from subfunctions to wrapper functions, _debounce to debounce
 # TODO - Add logging
